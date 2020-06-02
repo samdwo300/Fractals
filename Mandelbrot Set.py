398c84c525b6a60166da1a87bebc6c6d37eea339
Sam Dworetzky <samdwo300@gmail.com>
2020-06-02 19:01:05 -0400

    Update Mandelbrot Set.py

--- Mandelbrot Set.py
+++ Mandelbrot Set.py
@@ -19,25 +19,41 @@ import colorsys
 # For a given c this function returns True if c is in the Mandelbrot set for a
 #  given iteration threshhold and False otherwise
 def mandelbrot_sequence(a,b):
-    iterations = 10
+    iterations = 100
     c = complex(a,b)
     z = c
     for i in range(iterations):
-        if abs(z**2 + c) > 2 and i <= 1 and i<=2:
-            return (0,0,0)
-        elif abs(z**2 + c) > 2 and i <= 3 and i<=4:
-            return (255,48,48)
-        elif abs(z**2 + c) > 2 and i <= 5 and i<=6:
+        if abs(z**2 + c) > 2 and i<=10:
+            return (99,184,255)
+        elif abs(z**2 + c) > 2 and i >= 11 and i<=20:
             return (205,16,118)
             break
-        elif abs(z**2 + c) > 2 and i <= 7 and i<=8:
+        elif abs(z**2 + c) > 2 and i >= 21 and i<=30:
             return (139,10,80)
             break
-        elif abs(z**2 + c) > 2 and i <= 9 and i<= 10:
+        elif abs(z**2 + c) > 2 and i >= 31 and i<= 40:
             return (30,144,255)
-            break     
+            break  
+        elif abs(z**2 + c) > 2  and i >= 41 and i<= 50:
+            return (120,22,35)
+            break  
+        elif abs(z**2 + c) > 2  and i >= 51 and i<= 60:
+            return (100,144,255)
+            break    
+        elif abs(z**2 + c) > 2  and i >= 61 and i<= 70:
+            return (30,144,3)
+            break    
+        elif abs(z**2 + c) > 2  and i >= 71 and i<= 80:
+            return (49,40,20)
+            break    
+        elif abs(z**2 + c) > 2  and i >= 81 and i<= 90:
+            return (238,180,34)
+            break 
+        elif abs(z**2 + c) > 2  and i >= 91 and i<= 100:
+            return (50,180,200)
+            break    
         z = z**2 + c
-    return (0,0,0)
+    return (255,99,71)
 
  
     
@@ -54,17 +70,17 @@ for x in range(img.size[0]):
 
 # Crop image below
 
-width, height = img.size   # Get dimensions
-left = width
-top = height
-right = width/2
-bottom = height
+
+left = 0
+top = 100
+right = 325
+bottom = 400
 cropped = img.crop((left, top, right, bottom))
 
 cropped.show()
 # completing the given number of iterations 
-img.show() 
-img.save("out.jpg", "JPEG", quality=80, optimize=True, progressive=True)
+
+cropped.save("out.jpg", "JPEG", quality=80, optimize=True, progressive=True)
 
 
 
